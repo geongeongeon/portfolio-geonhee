0eb7b2032bdfc0d9c47e51d49f8938ad31749528
geongeongeon <leegeonhee0204@gmail.com>
2025-02-14 19:54:10 +0900

    Docker로 프로젝트 빌드

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 	kotlin("plugin.jpa") version "1.8.22"
 }
 
-group = "com.yongback"
+group = "com.geonhee"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
